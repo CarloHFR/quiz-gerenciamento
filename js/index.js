cee12badb68c8ce04f718dfb594e3b67540db410
CarloHFR <carlohenrique78@gmail.com>
2021-10-10 16:09:32 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,117 @@
+var score = 0;
+var current_question_id = 1;
+var current_question = questions[current_question_id];
+var num_of_questions = Object.keys(questions).length;
+
+
+var start_game = function()
+{
+    $('.content-modal').fadeOut();
+
+    setTimeout(function()
+    {
+        write_question_screen();
+        $('.content-modal').show();
+    }, 1000);
+}
+
+
+var restart_game = function()
+{
+    $('.content-modal').fadeOut();
+
+    score = 0;
+    current_question_id = 1;
+    current_question = questions[current_question_id];
+
+    setTimeout(function()
+    {
+        write_question_screen();
+        $('.content-modal').show();
+    }, 1000);
+}
+
+
+var get_next_question = function()
+{
+    setTimeout(function()
+    {
+        if(current_question_id < num_of_questions)
+        {
+            current_question_id += 1;
+            current_question = questions[current_question_id];
+            write_question_screen();
+            $('.content-modal').show();
+        }else
+        {
+            //end of game
+            write_final_screen();
+            $('.content-modal').show();
+        }     
+    }, 1000);
+}
+
+
+var check_answer = function(answer)
+{
+    var ans = $(answer).val();
+
+    $('.content-modal').fadeOut();
+
+    if(ans == current_question.correct_answer)
+    {
+        score += 1;
+        document.getElementById('success').play();
+    }else
+    {
+        document.getElementById('fail').play();
+    }
+
+    get_next_question();
+}; 
+
+
+var write_question_screen = function()
+{
+    modal = "<div class='modal-header'><h3 class='col-12 modal-title text-center'><span class='label label-warning' id='qid'></span>" + current_question.question + "</h3></div>";
+    modal += "<div class='modal-body'><div class='quiz' id='quiz' data-toggle='buttons'>";
+
+    if(current_question.type == "multi")
+    {
+        modal += "<button class='element-animation1 btn btn-lg btn-light btn-block text-info' value='a1' onclick='check_answer(this)'>" + current_question.answers.a1 + "</button>";
+        modal += "<button class='element-animation2 btn btn-lg btn-light btn-block text-info' value='a2' onclick='check_answer(this)'>" + current_question.answers.a2 + "</button>";
+        modal += "<button class='element-animation3 btn btn-lg btn-light btn-block text-info' value='a3' onclick='check_answer(this)'>" + current_question.answers.a3 + "</button>";
+        modal += "<button class='element-animation4 btn btn-lg btn-light btn-block text-info' value='a4' onclick='check_answer(this)'>" + current_question.answers.a4 + "</button>";
+    }else if(current_question.type == "vf")
+    {
+        modal += "<button class='element-animation1 btn btn-lg btn-light btn-block text-info' value='a1' onclick='check_answer(this)'>" + current_question.answers.a1 + "</button>";
+        modal += "<button class='element-animation2 btn btn-lg btn-light btn-block text-info' value='a2' onclick='check_answer(this)'>" + current_question.answers.a2 + "</button>";
+    }
+
+    modal += "</div></div><div class='modal-footer'><span class='score'>Score: " + score + "</span>";
+
+    $(".content-modal").html("");
+    $(".content-modal").html(modal);
+}; 
+
+
+var write_final_screen = function()
+{
+    if(score < 3)
+    {
+        final_message = "Que pena, você precisa estudar mais!";
+    }else if (score >= 3 && score <= 5)
+    {
+        final_message = "Parabéns, mas continue estudando!";
+    }else if (score >= 5)
+    {
+        final_message = "Parabéns, você conhece bem o assunto!";
+    }
+
+    modal = "<div class='modal-header'><h3 class='col-12 modal-title text-center'><span class='label label-warning' id='qid'></span>SCORE</h3></div>";
+    modal += "<div class='modal-body'><div class='final-score'>" + score + "/8</div></br><div class='final-message'>" + final_message + "</div></br><button class='element-animation1 btn btn-lg btn-light text-info' onclick='restart_game()'>Jogar Novamente</button></div>";
+    modal += "<div class='modal-footer text-muted'><span id='answer'></span>";
+
+    $(".content-modal").html("");
+    $(".content-modal").html(modal);
+}; 
---
+++ js/questions.js
@@ -0,0 +1,80 @@
+questions = {
+    "1": {
+        "question": "O que significa LAN ?",
+        "type": "multi",
+        "answers": {
+            "a1": "Local Area Network",
+            "a2": "Long Area Network",
+            "a3": "Large Area Network",
+            "a4": "Lean Area Network"
+        },
+        "correct_answer": "a1"
+    },
+    "2": {
+        "question": "Qual é a definição de LAN?",
+        "type": "multi",
+        "answers": {
+            "a1": "É uma linguagem de programação",
+            "a2": "É uma rede local de computadores",
+	    "a3": "É uma rede global de computadores",
+            "a4": "É um componente do computador"
+        },
+        "correct_answer": "a2"
+    },
+    "3": {
+        "question": "Quais os quatro tipos de topologias utilizadas para construir LAN?",
+        "type": "multi",
+        "answers": {
+            "a1": "Estrela, Barramento, Triangular, Anel",
+            "a2": "Árvore, Estrela, Anel e Barramento",
+	    "a3": "Barramento, Triangular, Árvore, Estrela",
+            "a4": "Barramento, Anel, Estrela e Malha"
+        },
+        "correct_answer": "a4"
+    },
+    "4": {
+        "question": "Uma conexão do tipo LAN é realizada por dispositivos conectados globalmente",
+        "type": "vf",
+        "answers": {
+            "a1": "Verdadeiro",
+            "a2": "Falso"
+        },
+        "correct_answer": "a2"
+    },
+    "5": {
+        "question": "A união de várias redes LAN formam uma rede WAN",
+        "type": "vf",
+        "answers": {
+            "a1": "Verdadeiro",
+            "a2": "Falso"
+        },
+        "correct_answer": "a1"
+    },
+    "6": {
+        "question": "O limitador da rede LAN é uma faixa de IP restrita à mesma, com uma máscara de rede comum",
+        "type": "vf",
+        "answers": {
+            "a1": "Verdadeiro",
+            "a2": "Falso"
+        },
+        "correct_answer": "a1"
+    },
+    "7": {
+        "question": "Cada dispositivo conectado em LAN é chamado de nó na rede",
+        "type": "vf",
+        "answers": {
+            "a1": "Verdadeiro",
+            "a2": "Falso"
+        },
+        "correct_answer": "a1"
+    },
+    "8": {
+        "question": "Possuir uma rede local dificulta a eficiência e a produtividade, aumento os custos no local de trabalho",
+        "type": "vf",
+        "answers": {
+            "a1": "Verdadeiro",
+            "a2": "Falso"
+        },
+        "correct_answer": "a2"
+    }
+};
