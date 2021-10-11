ed5b7043e26cb10ba2daa4c315af54f062dc7405
CarloHFR <carlohenrique78@gmail.com>
2021-10-11 11:52:47 -0300

    adicionei a pagina de conteudo

--- js/index.js
+++ js/index.js
@@ -1,3 +1,28 @@
+// ==========================
+// Back to top
+// ==========================
+var offset = 200;
+var duration = 500;
+$(window).scroll(function() {
+    if ($(this).scrollTop() > offset) {
+    $('.back-to-top').fadeIn(400);
+    } else {
+    $('.back-to-top').fadeOut(400);
+    }
+});
+
+$('.back-to-top').on('click',function(event) {
+    event.preventDefault();
+    $('html, body').animate({
+    scrollTop: 0
+    }, 600);
+    return false;
+})
+
+
+// ==========================
+// Game
+// ==========================
 var score = 0;
 var current_question_id = 1;
 var current_question = questions[current_question_id];
