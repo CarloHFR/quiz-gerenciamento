52358ac8367c637316392f2b70effe18fd1e474e
CarloHFR <carlohenrique78@gmail.com>
2021-10-17 17:55:15 -0300

    Adicionado as perguntas e o conteudo

--- js/index.js
+++ js/index.js
@@ -122,19 +122,19 @@ var write_question_screen = function()
 
 var write_final_screen = function()
 {
-    if(score < 3)
+    if(score <= 3)
     {
         final_message = "Que pena, você precisa estudar mais!";
-    }else if (score >= 3 && score <= 5)
+    }else if (score > 3 && score <= 6)
     {
         final_message = "Parabéns, mas continue estudando!";
-    }else if (score >= 5)
+    }else if (score > 6)
     {
         final_message = "Parabéns, você conhece bem o assunto!";
     }
 
     modal = "<div class='modal-header'><h3 class='col-12 modal-title text-center'><span class='label label-warning' id='qid'></span>SCORE</h3></div>";
-    modal += "<div class='modal-body'><div class='final-score'>" + score + "/8</div></br><div class='final-message'>" + final_message + "</div></br><button class='element-animation1 btn btn-lg btn-light text-info' onclick='restart_game()'>Jogar Novamente</button></div>";
+    modal += "<div class='modal-body'><div class='final-score'>" + score + "/9</div></br><div class='final-message'>" + final_message + "</div></br><button class='element-animation1 btn btn-lg btn-light text-info' onclick='restart_game()'>Jogar Novamente</button></div>";
     modal += "<div class='modal-footer text-muted'><span id='answer'></span>";
 
     $(".content-modal").html("");
--- js/questions.js
+++ js/questions.js
@@ -1,39 +1,15 @@
 questions = {
     "1": {
-        "question": "O que significa LAN ?",
-        "type": "multi",
+        "question": "O objetivo do Gerenciamento de eventos é prover a capacidade de detectar eventos, analisá-los e determinar ações de controle apropriadas.",
+        "type": "vf",
         "answers": {
-            "a1": "Local Area Network",
-            "a2": "Long Area Network",
-            "a3": "Large Area Network",
-            "a4": "Lean Area Network"
+            "a1": "Verdadeiro",
+            "a2": "Falso"
         },
         "correct_answer": "a1"
     },
     "2": {
-        "question": "Qual é a definição de LAN?",
-        "type": "multi",
-        "answers": {
-            "a1": "É uma linguagem de programação",
-            "a2": "É uma rede local de computadores",
-	    "a3": "É uma rede global de computadores",
-            "a4": "É um componente do computador"
-        },
-        "correct_answer": "a2"
-    },
-    "3": {
-        "question": "Quais os quatro tipos de topologias utilizadas para construir LAN?",
-        "type": "multi",
-        "answers": {
-            "a1": "Estrela, Barramento, Triangular, Anel",
-            "a2": "Árvore, Estrela, Anel e Barramento",
-	    "a3": "Barramento, Triangular, Árvore, Estrela",
-            "a4": "Barramento, Anel, Estrela e Malha"
-        },
-        "correct_answer": "a4"
-    },
-    "4": {
-        "question": "Uma conexão do tipo LAN é realizada por dispositivos conectados globalmente",
+        "question": "Eventos anormais indicam quando o sistema não apresenta erro mas requer atenção.",
         "type": "vf",
         "answers": {
             "a1": "Verdadeiro",
@@ -41,8 +17,30 @@ questions = {
         },
         "correct_answer": "a2"
     },
+    "3": {
+        "question": "Qual destes não é o papel do gerente de incidentes?",
+        "type": "multi",
+        "answers": {
+            "a1": "Buscar a eficiência e eficácia dos processos.",
+            "a2": "Produzir informações gerenciais, como relatórios de atendimento e de tipos de incidentes.",
+	        "a3": "Comunicar informações para quem for necessário, investigar e resolver eventos.",
+            "a4": "Gerenciar o trabalho das equipes de suporte de primeiro e segundo nível."
+        },
+        "correct_answer": "a3"
+    },
+    "4": {
+        "question": "Quais as atividades primárias do gerenciamento de problemas?",
+        "type": "multi",
+        "answers": {
+            "a1": "Controle de Problemas, Controle de Erros, Gerenciamento de Configuração, Controle de incidentes.",
+            "a2": "Controle de Erros, Gerenciamento de Mudanças, Controle de acesso, Gerenciamento Proativo de Problemas.",
+            "a3": "Controle de Problemas, Controle de Erros, Gerenciamento Proativo de Problemas, Finalização da revisão dos Problemas Graves.",
+            "a4": "Controle de Problemas, Gerenciamento de Configurações, Gerenciamento de Liberação, Finalização da revisão dos Problemas Graves."
+        },
+        "correct_answer": "a3"
+    },
     "5": {
-        "question": "A união de várias redes LAN formam uma rede WAN",
+        "question": "As principais atividades do Controle de Problemas são: Identificação e registro, classificação, Investigação e diagnóstico, Requisição de mudança, solução e fechamento.",
         "type": "vf",
         "answers": {
             "a1": "Verdadeiro",
@@ -51,30 +49,43 @@ questions = {
         "correct_answer": "a1"
     },
     "6": {
-        "question": "O limitador da rede LAN é uma faixa de IP restrita à mesma, com uma máscara de rede comum",
-        "type": "vf",
-        "answers": {
-            "a1": "Verdadeiro",
-            "a2": "Falso"
-        },
-        "correct_answer": "a1"
-    },
-    "7": {
-        "question": "Cada dispositivo conectado em LAN é chamado de nó na rede",
-        "type": "vf",
-        "answers": {
-            "a1": "Verdadeiro",
-            "a2": "Falso"
-        },
-        "correct_answer": "a1"
-    },
-    "8": {
-        "question": "Possuir uma rede local dificulta a eficiência e a produtividade, aumento os custos no local de trabalho",
+        "question": "O processo de cumprimento de requisição geralmente contempla demandas que representam grandes alterações que são de alto custo e risco, e que ocorrem com frequência.",
         "type": "vf",
         "answers": {
             "a1": "Verdadeiro",
             "a2": "Falso"
         },
         "correct_answer": "a2"
+    },
+    "7": {
+        "question": "Qual desses não é um conceito de gerenciamento de acesso.",
+        "type": "multi",
+        "answers": {
+            "a1": "Access",
+            "a2": "Identity",
+            "a3": "Rights",
+            "a4": "Control"
+        },
+        "correct_answer": "a4"
+    },
+    "8": {
+        "question": "O gerenciamento de acesso afeta as permissões de um usuário.",
+        "type": "vf",
+        "answers": {
+            "a1": "Verdadeiro",
+            "a2": "Falso"
+        },
+        "correct_answer": "a1"
+    },
+    "9": {
+        "question": "Qual dessas é uma característica da central de serviços centralizada?",
+        "type": "multi",
+        "answers": {
+            "a1": "Padronizar os procedimentos e métodos de atendimento ao cliente.",
+            "a2": "Desenvolver procedimentos de atendimento separados para cada unidade.",
+            "a3": "Fragmentar a central de serviços em várias unidades separadas de atendimento.",
+            "a4": "Gerar melhora no atendimento ao cliente porém aumentar os custos operacionais."
+        },
+        "correct_answer": "a1"
     }
 };
